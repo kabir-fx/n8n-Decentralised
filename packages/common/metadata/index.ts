a9b8df315ef5ff6e2a7bc961bb3481af81765ea0
Kabir <vatshivam49888@gmail.com>
2026-01-10 21:01:42 +0530

    Feat: Improve typecasting for asset type.

--- packages/common/metadata/index.ts
+++ packages/common/metadata/index.ts
@@ -1,35 +1,34 @@
-import type { SUPPORTED_ASSETS } from "../supported-components"
+import type { SUPPORTED_ASSETS } from "../supported-components";
 
 // Properties attached to a Price Trigger node
 export type PriceTriggerMetadata = {
-    // SOL, BTC, ETH, etc
-    asset: typeof SUPPORTED_ASSETS
+  // SOL, BTC, ETH, etc
+  asset: (typeof SUPPORTED_ASSETS)[number];
 
-    // Price of the asset
-    price: number
-}
+  // Price of the asset
+  price: number;
+};
 
 // Properties attached to the action nodes
 export type ActionNodeMetadata = {
-    type: "LONG" | "SHORT",
-    qty: number,
-    asset: typeof SUPPORTED_ASSETS
-}
+  type: "LONG" | "SHORT";
+  qty: number;
+  asset: (typeof SUPPORTED_ASSETS)[number];
+};
 
 // Properties attached to a Time trigger node
 export type TimerTriggerMetadata = {
-    // Seconds until this triggers acts
-    time: number
-}
+  // Seconds until this triggers acts
+  time: number;
+};
 
 // Properties attached to a Asset Transfer node
 export type AssetTransferMetadata = {
-    // SOL, BTC, ETH, etc
-    asset: typeof SUPPORTED_ASSETS
+  // SOL, BTC, ETH, etc
+  asset: (typeof SUPPORTED_ASSETS)[number];
 
-    // Amount of asset to transfer
-    amount_to_transfer: number
+  // Amount of asset to transfer
+  amount_to_transfer: number;
 
-    receiver_address: string
-}
-
+  receiver_address: string;
+};
