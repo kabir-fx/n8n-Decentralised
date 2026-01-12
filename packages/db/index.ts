950d257812705fd412db7809caeefa5f83704c34
Kabir <vatshivam49888@gmail.com>
2026-01-12 23:36:05 +0530

    feat: Update DB schema to allow unique nullable wallet address.

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,165 +1,178 @@
-import mongoose, { Schema } from "mongoose"
+import mongoose, { Schema } from "mongoose";
 
 // ============================================
 // USER SCHEMA
 // ============================================
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     walletAddress: {
-        type: String,
-        required: true,
-        unique: true
-    }
-}, {
-    timestamps: true
-});
+      type: String,
+      required: false,
+      unique: true,
+      sparse: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 export const UserModel = mongoose.model("User", userSchema);
 
 // ============================================
 // EDGES SCHEMA
 // ============================================
-const edgesSchema = new Schema({
+const edgesSchema = new Schema(
+  {
     edgeId: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     source: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     target: {
-        type: String,
-        required: true
-    }
-}, {
-    _id: false
-})
+      type: String,
+      required: true,
+    },
+  },
+  {
+    _id: false,
+  }
+);
 
 // ============================================
 // NODE PROPERTIES SCHEMA
 // ============================================
-const nodePropertiesSchema = new Schema({
+const nodePropertiesSchema = new Schema(
+  {
     nodeId: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     node: {
-        type: mongoose.Types.ObjectId,
-        ref: 'Node',
-        required: true
+      type: mongoose.Types.ObjectId,
+      ref: "Node",
+      required: true,
     },
     metadata: Schema.Types.Mixed,
     position: {
-        x: {
-            type: Number,
-            required: true
-        },
-        y: {
-            type: Number,
-            required: true
-        },
+      x: {
+        type: Number,
+        required: true,
+      },
+      y: {
+        type: Number,
+        required: true,
+      },
     },
-    credentials: Schema.Types.Mixed
-}, {
-    _id: false
-});
+    credentials: Schema.Types.Mixed,
+  },
+  {
+    _id: false,
+  }
+);
 
 // ============================================
 // WORKFLOW SCHEMA
 // ============================================
-const workflowSchema = new Schema({
+const workflowSchema = new Schema(
+  {
     userId: {
-        type: mongoose.Types.ObjectId,
-        ref: 'User',
-        required: true
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     isActive: {
-        type: Boolean,
+      type: Boolean,
     },
     edges: [edgesSchema],
-    nodeProperties: [nodePropertiesSchema]
-}, {
-    timestamps: true
-})
+    nodeProperties: [nodePropertiesSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
 export const WorkflowModel = mongoose.model("Workflow", workflowSchema);
 
 // ============================================
 // NODE PROPERTIES SCHEMA
 // ============================================
 const nodeSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    type: {
-        type: String,
-        enum: ["ACTION", "TRIGGER"],
-        required: true
-    }
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["ACTION", "TRIGGER"],
+    required: true,
+  },
+});
 export const NodeModel = mongoose.model("Node", nodeSchema);
 
 // ============================================
 // EXECUTION SCHEMA
 // ============================================
 const executionSchema = new Schema({
-    workflowId: {
-        type: mongoose.Types.ObjectId,
-        ref: 'Workflow',
-        required: true
-    },
-    status: {
-        type: String,
-        enum: ["PENDING", "FINISHED", "CANCELLED", "FAILED"],
-        required: true
-    },
-    depositTxSignature: {
-        type: String,
-        required: true
-    },
-    executionTxSignature: String,
-    escrow_pda: {
-        type: String,
-        required: true
-    },
-    vault_pda: {
-        type: String,
-        required: true
-    },
-    maker_pubkey: {
-        type: String,
-        required: true
-    },
-    destination_pubkey: {
-        type: String,
-        required: true
-    },
-    amount_lamports: {
-        type: Number,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        required: true
-    },
-    StartTime: Date,
-    endTime: Date
-})
+  workflowId: {
+    type: mongoose.Types.ObjectId,
+    ref: "Workflow",
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["PENDING", "FINISHED", "CANCELLED", "FAILED"],
+    required: true,
+  },
+  depositTxSignature: {
+    type: String,
+    required: true,
+  },
+  executionTxSignature: String,
+  escrow_pda: {
+    type: String,
+    required: true,
+  },
+  vault_pda: {
+    type: String,
+    required: true,
+  },
+  maker_pubkey: {
+    type: String,
+    required: true,
+  },
+  destination_pubkey: {
+    type: String,
+    required: true,
+  },
+  amount_lamports: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  StartTime: Date,
+  endTime: Date,
+});
 export const ExecutionModel = mongoose.model("Execution", executionSchema);
